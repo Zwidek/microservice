66063201790222dbf1454b6c9d50cf728fa79861
Mgr <zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.com>
2022-10-02 00:41:26 +0200

    Dodanie obsługi kolejkowania wiadmości przy użyciu RabbitMQ (:

--- customer/src/main/java/com/zwidek/customer/CustomerController.java
+++ customer/src/main/java/com/zwidek/customer/CustomerController.java
@@ -2,6 +2,8 @@ package com.zwidek.customer;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +17,27 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    private final RabbitTemplate rabbitTemplate;
+
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest request) {
+        rabbitTemplate.convertAndSend("registration", welcomeMessage(request));
         log.info("New customer registration {}", request);
         customerService.registerCustomer(request);
+    }
+
+    @RabbitListener(queues = "registration")
+    public void rabbitListener(String message) {
+        System.out.println(message);
+    }
+
+    private StringBuilder welcomeMessage(CustomerRegistrationRequest request) {
+        StringBuilder sb = new StringBuilder();
+        return sb.append("Hello ")
+                .append(request.firstName())
+                .append("!\n")
+                .append("You address email is: ")
+                .append(request.email());
     }
 }
 
