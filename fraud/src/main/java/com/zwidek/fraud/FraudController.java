21bc402b7d669b189a4aade0144a48187b63b8d0
Mgr <zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.comgit config --global user.email zwidekxd@gmail.com>
2022-10-02 02:00:22 +0200

    Dodanie logowania oraz monitoring za pomocą zipkina

--- fraud/src/main/java/com/zwidek/fraud/FraudController.java
+++ fraud/src/main/java/com/zwidek/fraud/FraudController.java
@@ -2,6 +2,8 @@ package com.zwidek.fraud;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
-        log.info("Fraud check request for customer {}", customerId);
+        LOG.info("Fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
